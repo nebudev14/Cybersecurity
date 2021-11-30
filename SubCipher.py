1772eb6e5f16c9663439e940d1b6a7d9e78af3a8
warren yun <xxwarrenhdxx@gmail.com>
2021-11-30 07:55:47 -0500

    rewrite

--- SubCipher.py
+++ SubCipher.py
@@ -1,13 +1,23 @@
+import base64
+import hashlib
+
 # Substitution Cipher in Python
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = 'fcpevqkzgmtrayonujdlwhbxsi'
 
 def encrypt(message):
     encrypted = ""
     for letter in message:
-        if ord(letter)-95 <= 13:
-            encrypted += chr(ord(letter)+13)
-
-        elif ord(letter)-95 >= 13:
-            encrypted += chr(ord(letter)-13)
+        if letter.lower() in alphabet:
+            encrypted += key[alphabet.find(letter.lower())]
     return encrypted
 
-print(encrypt("hello")) # returns URYYB
+def decrypt(message):
+    decrypted = ""
+    for letter in message:
+        if letter.lower() in key:
+            decrypted += alphabet[key.find(letter.lower())]
+    return decrypted
+
+print(encrypt("AAHHHHHHHHHHHHHHHHHHH"))
+print(decrypt(encrypt("hello")))
